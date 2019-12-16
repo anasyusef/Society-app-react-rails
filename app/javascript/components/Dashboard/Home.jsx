ed4c1b44753a5a0ddd81dd831e3f01f5ac6fffd3
Anas Yousef <ay00261@surrey.ac.uk>
2019-12-16 19:44:09 +0000

    Removing unused imports

--- app/javascript/components/Dashboard/Home.jsx
+++ app/javascript/components/Dashboard/Home.jsx
@@ -2,21 +2,15 @@ import React, {useEffect} from 'react';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
-import Orders from './Orders';
+import JoinSocietiesCard from './JoinedSocietiesCard'
 import Auth from 'j-toker'
-import NextEca from './NextEca'
+import TotalSocieties from './TotalSocieties'
 import useStyles from './styles';
 import Dashboard from './Dashboard'
 
@@ -34,13 +28,13 @@ export default function Home() {
             {/* Recent Deposits */}
             <Grid item xs={12} md={4} lg={3}>
                 <Paper className={fixedHeightPaper}>
-                <NextEca />
+                <TotalSocieties />
                 </Paper>
             </Grid>
             {/* Recent Orders */}
             <Grid item xs={12}>
                 <Paper className={classes.paper}>
-                <Orders />
+                    <JoinSocietiesCard />
                 </Paper>
             </Grid>
         </Grid>
