4f1b1150de6e07c7041050c5f5b0b2090a07bd37
Anas Yousef <ay00261@surrey.ac.uk>
2019-12-17 09:58:59 +0000

    Added tests to Society model. All pass

--- app/models/society.rb
+++ app/models/society.rb
@@ -1,9 +1,23 @@
 class Society < ApplicationRecord
     before_save :default_values
-    validates :name, :max_people, :location, :brief_description, presence: true
+
+    validates :max_people, :location, :brief_description, presence: true
+    validates :name, presence: true, uniqueness: true
+    validate :society_cannot_be_member_role
+
     belongs_to :user
     belongs_to :schedule
     has_many :registrations
+
+    def society_cannot_be_member_role
+        if !self.user.nil?
+            if !self.user.role.nil?
+                if  self.user.role.name != "Society Administrator"
+                    errors.add(:user, " must be a Society Administrator ")
+                end
+            end
+        end
+    end
 
     def default_values
         self.is_active ||= false
--- test/models/society_test.rb
+++ test/models/society_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class SocietyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save if society is not in the table" do
+    @society = Society.new(name: "Football", max_people: 11, location: "Surrey Sports Park", brief_description: "Lorem ipsum", essentials: "Football kit")
+    @user = User.new(first_name: "Foo", last_name: "Bar", email: "foobar@example.com", password: "random", encrypted_password: "random")
+    @role = Role.new(name: "Society Administrator")
+    @role.save
+    @user.role = @role
+    @schedule = Schedule.new(day: "Tuesday", start_time: "14:00".to_time, end_time: "16:00".to_time)
+    @society.schedule = @schedule
+    @society.user = @user
+    assert @society.save!
+  end
+
+  test "should not save if society is in the table" do
+    @society = Society.new(name: "Football", max_people: 11, location: "Surrey Sports Park", brief_description: "Lorem ipsum", essentials: "Football kit")
+    @user = User.new(first_name: "Foo", last_name: "Bar", email: "foobar@example.com", password: "random", encrypted_password: "random")
+    @role = Role.new(name: "Society Administrator")
+    @role.save
+    @user.role = @role
+    @schedule = Schedule.new(day: "Tuesday", start_time: "14:00".to_time, end_time: "16:00".to_time)
+    @society.schedule = @schedule
+    @society.user = @user
+    @society.save
+
+    refute Society.new(name: "Football", max_people: 11, location: "Surrey Sports Park", brief_description: "Lorem ipsum", essentials: "Football kit", user: @user, schedule: @schedule).valid?
+  end
+
+  test "should not save if society is blank" do
+    refute Society.new.valid?
+  end
+
+  test "should not save if user from society is not assigned" do
+    @schedule = Schedule.new(day: "Tuesday", start_time: "14:00".to_time, end_time: "16:00".to_time)
+    @schedule.save
+    refute Society.new(name: "Football", max_people: 11, location: "Surrey Sports Park", brief_description: "Lorem ipsum", essentials: "Football kit", schedule: @schedule).valid?
+  end
+
+  test "should not save if schedule from society is not assigned" do
+    @user = User.new(first_name: "Foo", last_name: "Bar", email: "foobar@example.com", password: "random", encrypted_password: "random")
+    @role = Role.new(name: "Society Administrator")
+    @user.save
+    refute Society.new(name: "Football", max_people: 11, location: "Surrey Sports Park", brief_description: "Lorem ipsum", essentials: "Football kit", user: @user).valid?
+  end
+
+  test "should not save if name  max_people location brief_description are not assigned" do
+    @user = User.new(first_name: "Foo", last_name: "Bar", email: "foobar@example.com", password: "random", encrypted_password: "random")
+    @role = Role.new(name: "Society Administrator")
+    @user.save
+    @schedule = Schedule.new(day: "Tuesday", start_time: "14:00".to_time, end_time: "16:00".to_time)
+    @schedule.save
+    refute Society.new(essentials: "Football kit", user: @user, schedule: @schedule).valid?
+  end
+
 end
